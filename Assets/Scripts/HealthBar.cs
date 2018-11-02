2a45072b1002a311b36e446023e5e8d61d4bd00f
DmitriiSukhomlinov <sukhomlinovdmitrii@yandex.ru>
2018-11-03 00:14:19 +0700

    Health Bar visibility

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -11,6 +11,10 @@ public class HealthBar : MonoBehaviour {
     private RectTransform myCanvas;
     private Image imageComponent;
 
+    private float TIME_TO_VISIBILITY = 0.5f;
+
+    private float timeSinceDamage = 1f;
+
     // Use this for initialization
     void Start () {
         myCanvas = GetComponentInParent<Canvas>().GetComponent<RectTransform>();
@@ -19,6 +23,15 @@ public class HealthBar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void LateUpdate() {
+        HealthBarMooving();
+
+        timeSinceDamage = Mathf.Min(1f, timeSinceDamage + Time.deltaTime);
+        if (timeSinceDamage == 1f) {
+            imageComponent.color = new Color(imageComponent.color.r, imageComponent.color.g, imageComponent.color.b, Mathf.Max(0f, imageComponent.color.a - Time.deltaTime));
+        }
+    }
+
+    private void HealthBarMooving () {
         Vector3 viewportPoint = Camera.main.WorldToViewportPoint(objectToFollow.position);
 
         viewportPoint -= 0.5f * Vector3.one;
@@ -36,7 +49,9 @@ public class HealthBar : MonoBehaviour {
 
             return;
         }
+        timeSinceDamage = 0f;
         healthValue--;
         imageComponent.fillAmount = healthValue * 0.01f;
+        imageComponent.color = new Color(imageComponent.color.r, imageComponent.color.g, imageComponent.color.b, Mathf.Min(1f, imageComponent.color.a + Time.deltaTime * 3));
     }
 }
